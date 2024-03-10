7daab32fb4fbb7875146e445ab5e1775e7923f6f
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-03-10 18:26:27 +0000

    Hero Section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,9 +3,9 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hi, I'm Narinder</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <span className="hero--section-title--color">Front End</span>{" "}
             <br />
             Developer
           </h1>
