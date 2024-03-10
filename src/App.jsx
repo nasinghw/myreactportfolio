eadcba4fb1886aaa6167feeeb10d5d7f729211cd
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-03-10 19:13:43 +0000

    App JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
-// import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
